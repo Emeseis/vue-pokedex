dfdc800ee45dd0c9657281b13f252ef404bc1fb0
Maycon Negrini <maycon128@hotmail.com>
2022-11-10 06:44:16 -0300

    evolution-chart onclicknode change pokemon

--- src/components/EvolutionChart.vue
+++ src/components/EvolutionChart.vue
@@ -7,27 +7,22 @@ import OrgChart from '@balkangraph/orgchart.js'
 export default {
   name: 'tree',
   props: ['nodes'],
-  data() { return {} },
-  methods: {    
-    zerofy(value) {
-      if (!value) return;
-      let zeroes = new Array(4).join("0");
-      return (zeroes + value).slice(-3);
-    },
+  data() { return { chart: '' } },
+  methods: {
     myTree(domEl, x) {
       OrgChart.templates.customTemplate = Object.assign({}, OrgChart.templates.diva);
       OrgChart.templates.customTemplate.svg = '<svg id="tree2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="background-color:transparent;display:block;" width="{w}" height="{h}" viewBox="{viewBox}">{content}</svg>';
-      OrgChart.templates.customTemplate.size = [144, 244];
-      OrgChart.templates.customTemplate.node = "";
+      OrgChart.templates.customTemplate.size = [144, 250];
+      OrgChart.templates.customTemplate.node = '<div style="width: 144px; height: 250px"></div>';
       OrgChart.templates.customTemplate.field_0 = 
       '<text class="id-text" x="72" y="182">#{val}</text>';
       OrgChart.templates.customTemplate.field_1 = 
-      `<text class="name-anchor" x="72" y="207">{val}</text>`;
+      `<text class="name-text" x="72" y="207">{val}</text>`;
       OrgChart.templates.customTemplate.field_2 = '{val}'
       OrgChart.templates.customTemplate.link_field_0 = 
-      '<g class="arrow-text"><text x="0" y="40">({val})</text></g>';
+      '<g class="arrow-text"><text x="0" y="35">({val})</text></g>';
       OrgChart.templates.customTemplate.img_0 = 
-      '<image xlink:href="{val}" x="0" y="25" width="144" height="144"/>';
+      '<image class="image-chart" xlink:href="{val}" x="0" y="25" width="144" height="144"/>';
       OrgChart.templates.customTemplate.link = 
       '<marker id="head" orient="auto" markerWidth="5" markerHeight="5" refX="5" refY="5" viewBox="0 0 10 10">' +
         '<polyline points="0,5 5,2.5 0,0" fill="none" stroke-width="1" stroke="black" stroke-linecap="round" transform="matrix(1,0,0,1,1.6666666666666667,2.5)" stroke-linejoin="round"></polyline>' + 
@@ -40,8 +35,7 @@ export default {
         mouseScrool: OrgChart.action.none,
         enableDragDrop: false,
         nodeMouseClick: OrgChart.none,
-        interactive: false,
-        sticky: false,
+        interactive: true,
         enableSearch: false,
         scaleInitial: this.nodes[0].name == "Eevee" ? 0.9 : 1,
         nodes: x,
@@ -55,10 +49,18 @@ export default {
           img_0: "img"
         },
       });
+      this.chart.onNodeClick(args => {
+        let targetClass = args.event.target.classList.value;
+        if (targetClass == "name-text" || targetClass == "image-chart") this.changePokemon({ id: args.node.id });
+      });
+
       let height = ((this.nodes[this.nodes.length-1].level)*300)+'px';
       let orgChart = document.getElementById('tree');
       orgChart.style.height = height;
-    }
+    },
+    changePokemon(pokemon) {
+      this.$emit('changePokemon', pokemon);
+    },
   },
   mounted() {
     this.myTree(this.$refs.tree, this.nodes);
@@ -133,15 +135,25 @@ export default {
   font-family: "Fira Sans";
   text-anchor: middle;
 }
-.name-anchor {
+.name-text {
   font-size: 1rem;
   font-family: "Fira Sans";
   font-weight: bold;
   fill: #2769be;
   text-anchor: middle;
   text-decoration: none;
+  transition: fill .2s;
 }
-.arrow-text > text {
+.name-text:hover {
+  fill: #a42121 !important;
+  cursor: pointer;
+  text-decoration: underline;
+  outline: 0;
+}
+.image-chart:hover {
+  cursor: pointer;
+}
+.arrow-text {
   font-size: 14px;
   font-family: "Fira Sans";
   text-anchor: middle;
